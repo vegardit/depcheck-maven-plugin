500022253a5af52ee63f75709012b5f9b6f52ccf
sebthom <sebthom@users.noreply.github.com>
2021-03-13 19:49:42 +0100

    trigger release

--- .ci/release-trigger.sh
+++ .ci/release-trigger.sh
@@ -1,6 +1,6 @@
 # this file is evaluated by .ci/build.sh
 
-POM_CURRENT_VERSION="0.0.0-SNAPSHOT" # perform release if pom.xml matches this version
+POM_CURRENT_VERSION="1.0.0-SNAPSHOT" # perform release if pom.xml matches this version
 POM_RELEASE_VERSION="1.0.0" # next release version
 
 DRY_RUN=false # is dry run?
